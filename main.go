1e926527e76df270a2abdccd0da62e3e3a24f56d
omerzamir <zamir98@gmail.com>
2019-04-03 10:30:11 +0300

    basic server
    Signed-off-by: omerzamir <zamir98@gmail.com>

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	// Disable Console Color
+	gin.DisableConsoleColor()
+	r := gin.Default()
+
+	// Ping test
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	return r
+}
+
+func main() {
+	router := setupRouter()
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+}
