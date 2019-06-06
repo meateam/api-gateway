44cdcbd6cae02b1a1a6e7a6602a463d4a0198e39
Yonatan <yonatald@gmail.com>
2019-06-06 08:47:39 +0300

    fix tokenValid check bug
    Signed-off-by: Yonatan <yonatald@gmail.com>

--- auth.go
+++ auth.go
@@ -50,41 +50,48 @@ func authRequired(c *gin.Context) {
 		return
 	}
 
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		// Check type assertion.
-		// For some reason can't convert directly to int64
-		iat, ok := claims["iat"].(float64)
-		if !ok {
-			redirectToAuthService(c)
-			return
-		}
+	claims, ok := token.Claims.(jwt.MapClaims)
 
-		passed := time.Since(time.Unix(int64(iat), 0))
-
-		// Token expired
-		if time.Hour*24 < passed {
-			redirectToAuthService(c)
-			return
-		}
-
-		// Check type assertion
-		id, idOk := claims["id"].(string)
-		firstName, firstNameOk := claims["firstName"].(string)
-		lastName, lastNameOk := claims["lastName"].(string)
-
-		// If any of the claims are invalid then redirect to authentication
-		if !idOk || !firstNameOk || !lastNameOk {
-			redirectToAuthService(c)
-			return
-		}
-
-		c.Set("User", user{
-			id:        id,
-			firstName: firstName,
-			lastName:  lastName,
-		})
+	if !ok || !token.Valid {
+		redirectToAuthService(c)
+		return
 	}
+
+	// Check type assertion.
+	// For some reason can't convert directly to int64
+	iat, ok := claims["iat"].(float64)
+	if !ok {
+		redirectToAuthService(c)
+		return
+	}
+
+	passed := time.Since(time.Unix(int64(iat), 0))
+
+	// Token expired
+	if time.Hour*24 < passed {
+		redirectToAuthService(c)
+		return
+	}
+
+	// Check type assertion
+	id, idOk := claims["id"].(string)
+	firstName, firstNameOk := claims["firstName"].(string)
+	lastName, lastNameOk := claims["lastName"].(string)
+
+	// If any of the claims are invalid then redirect to authentication
+	if !idOk || !firstNameOk || !lastNameOk {
+		redirectToAuthService(c)
+		return
+	}
+
+	c.Set("User", user{
+		id:        id,
+		firstName: firstName,
+		lastName:  lastName,
+	})
+
 	c.Next()
+
 }
 
 func redirectToAuthService(c *gin.Context) {
