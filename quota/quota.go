f89d858155cc3a75f3301667b7f9992212615f45
omerzamir <zamir98@gmail.com>
2019-08-28 10:11:19 +0300

    added route and refactored
    Signed-off-by: omerzamir <zamir98@gmail.com>

--- quota/quota.go
+++ quota/quota.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	gwruntime "github.com/grpc-ecosystem/grpc-gateway/runtime"
 	loggermiddleware "github.com/meateam/api-gateway/logger"
 	"github.com/meateam/api-gateway/user"
 	qpb "github.com/meateam/file-service/proto/quota"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
 )
 
 // Router is a structure that handles quota related requests.
@@ -38,12 +40,29 @@ func NewRouter(
 
 // Setup sets up r and intializes its routes under rg.
 func (r *Router) Setup(rg *gin.RouterGroup) {
-	rg.GET("/users/:id/quota", r.GetOwnerQuota)
+	rg.GET("/user/quota", r.GetOwnerQuota)
+	rg.GET("/users/:id/quota", r.GetQuotaByID)
 }
 
-// GetOwnerQuota is the request handler for GET /users/:id/quota
+// GetOwnerQuota is the request handler for GET /user/quota
 func (r *Router) GetOwnerQuota(c *gin.Context) {
+	reqUser := user.ExtractRequestUser(c)
+	if reqUser == nil {
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	r.handleGetQuota(c, reqUser.ID, reqUser.ID)
+}
+
+// GetQuotaByID is the request handler for GET /users/:id/quota
+func (r *Router) GetQuotaByID(c *gin.Context) {
 	ownerID := c.Param("id")
+
+	if ownerID == "" {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
 
 	reqUser := user.ExtractRequestUser(c)
 	if reqUser == nil {
@@ -51,7 +70,12 @@ func (r *Router) GetOwnerQuota(c *gin.Context) {
 		return
 	}
 
-	allowed := r.isAllowedToGetQuota(c, reqUser.ID, ownerID)
+	r.handleGetQuota(c, reqUser.ID, ownerID)
+}
+
+// handleGetQuota handles the request to the quota Service and the response to the client.
+func (r *Router) handleGetQuota(c *gin.Context, requesterID string, ownerID string) {
+	allowed := r.isAllowedToGetQuota(c, requesterID, ownerID)
 	if !allowed {
 		c.AbortWithStatus(http.StatusUnauthorized)
 		return
@@ -62,7 +86,8 @@ func (r *Router) GetOwnerQuota(c *gin.Context) {
 		&qpb.GetOwnerQuotaRequest{OwnerID: ownerID},
 	)
 	if err != nil {
-		loggermiddleware.LogError(r.logger, c.AbortWithError(http.StatusInternalServerError, err))
+		httpStatusCode := gwruntime.HTTPStatusFromCode(status.Code(err))
+		loggermiddleware.LogError(r.logger, c.AbortWithError(httpStatusCode, err))
 		return
 	}
 
