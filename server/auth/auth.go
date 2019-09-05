8b9e1746bd4a3962cce2363616c34cdfcd50871d
shahar-y <shya95@gmail.com>
2019-09-05 09:01:49 +0300

    renaming variable
    Signed-off-by: shahar-y <shya95@gmail.com>

--- server/auth/auth.go
+++ server/auth/auth.go
@@ -72,9 +72,9 @@ func (r *Router) Middleware(secret string, authURL string) gin.HandlerFunc {
 			return
 		}
 
-		// ct is the current transaction of the apm. adding the user id to the context.
-		ct := apm.TransactionFromContext(c.Request.Context())
-		ct.Context.SetUserID(id)
+		// The current transaction of the apm, adding the user id to the context.
+		currentTarnasction := apm.TransactionFromContext(c.Request.Context())
+		currentTarnasction.Context.SetUserID(id)
 
 		// Check type assertion.
 		// For some reason can't convert directly to int64
