4a1ce84db40f7ac7a5642de589770b5ceedf9313
Omer Zamir <zamir98@gmail.com>
2019-12-16 13:54:56 +0200

    fixed metricLogger and healthcheck
    Signed-off-by: Omer Zamir <zamir98@gmail.com>

--- server/router.go
+++ server/router.go
@@ -26,7 +26,7 @@ import (
 )
 
 const (
-	healtcheckRouter  = "/api/healtcheck"
+	healthcheckRoute  = "/api/healthcheck"
 	uploadRouteRegexp = "/api/upload.+"
 )
 
@@ -43,7 +43,7 @@ func NewRouter(logger *logrus.Logger) (*gin.Engine, []*grpc.ClientConn) {
 	// Setup logging, metrics, cors middlewares.
 	r.Use(
 		// Ignore logging healthcheck routes.
-		gin.LoggerWithWriter(gin.DefaultWriter, healtcheckRouter),
+		gin.LoggerWithWriter(gin.DefaultWriter, healthcheckRoute),
 		gin.Recovery(),
 		apmgin.Middleware(r),
 		cors.New(corsRouterConfig()),
@@ -51,7 +51,7 @@ func NewRouter(logger *logrus.Logger) (*gin.Engine, []*grpc.ClientConn) {
 		loggermiddleware.SetLogger(
 			&loggermiddleware.Config{
 				Logger:             logger,
-				SkipPath:           []string{healtcheckRouter},
+				SkipPath:           []string{healthcheckRoute},
 				SkipBodyPathRegexp: regexp.MustCompile(uploadRouteRegexp),
 			},
 		),
@@ -120,10 +120,16 @@ func NewRouter(logger *logrus.Logger) (*gin.Engine, []*grpc.ClientConn) {
 	pr := permission.NewRouter(permissionConn, fileConn, userConn, logger)
 	sr := search.NewRouter(searchConn, fileConn, permissionConn, logger)
 
-	metricsLogger := NewMetricsLogger()
-	// Authentication middleware on routes group.
+	middlewares := make([]gin.HandlerFunc, 0, 2)
+
 	authRequiredMiddleware := ar.Middleware(viper.GetString(configSecret), viper.GetString(configAuthURL))
-	authRequiredRoutesGroup := apiRoutesGroup.Group("/", authRequiredMiddleware, metricsLogger)
+	middlewares = append(middlewares, authRequiredMiddleware)
+
+	if metricsLogger := NewMetricsLogger(); metricsLogger != nil {
+		middlewares = append(middlewares, metricsLogger)
+	}
+	// Authentication middleware on routes group.
+	authRequiredRoutesGroup := apiRoutesGroup.Group("/", middlewares...)
 
 	// Initiate client connection to file service.
 	fr.Setup(authRequiredRoutesGroup)
