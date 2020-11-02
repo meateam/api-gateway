7af3cb5d19dcf03cc9f7d6fd495aacf916b163e1
Yosef Romano <yosef32@gmail.com>
2020-11-02 10:02:06 +0200

    shahar commit

--- file/doc.go
+++ file/doc.go
@@ -4,38 +4,3 @@ Handling is implemented with a HTTP router returned from NewRouter and setup its
 using Setup.
 */
 package file
-
-import (
-	fpb "github.com/meateam/file-service/proto/file"
-)
-
-// swagger:response filesResponse
-type filesResponse struct {
-	// in:body
-	Body []fpb.File
-}
-
-// swagger:response fileResponse
-type fileResponse struct {
-	// in:body
-	Body GetFileByIDResponse
-}
-
-
-
-// swagger:response idResponse
-type idResponse struct {
-	// array of ids
-	ID []string
-}
-
-
-
-// swagger:parameters idParam
-type IDParam struct {
-    // The id of the file
-    // unique:true
-	// in:url
-	// required:true
-    ID string `json:"id"`
-}
--- swagger/swagger.go
+++ swagger/swagger.go
@@ -1,3 +1,28 @@
+// Package classification Drive API.
+//
+// Terms Of Service:
+//
+//
+//     Schemes: http
+//     Host: localhost
+//     BasePath: /api
+//     Version: v1.3
+//     Contact: Drive team<drive.team@example.com> http://www.google.com
+//
+//     Consumes:
+//     - application/json
+//     - application/xml
+//
+//     Produces:
+//     - application/json
+//
+// swagger:meta
+package swagger
+
+import (
+	file "github.com/meateam/api-gateway/file"
+)
+
 /***
  *       ▄████████  ▄█   ▄█          ▄████████    ▄████████ 
  *      ███    ███ ███  ███         ███    ███   ███    ███ 
@@ -10,7 +35,7 @@
  *                      ▀                                   
  */
 
-// swagger:route GET /api/files files listFiles
+// swagger:route GET /files files listFiles
 //
 // List of files
 //
@@ -20,22 +45,155 @@
 // Responses:
 // 	200: filesResponse
 
-// swagger:route GET /api/files/{id} files file
+// swagger:parameters listFiles
+type filesRequest struct {
+	// The file parent
+	// unique:true
+	// in:url
+    Parent string 
+}
+
+// An array of files
+// swagger:response filesResponse
+type filesResponse struct {
+	// in:body
+	Body []file.GetFileByIDResponse
+}
+
+// swagger:route GET /files/{id} files file
 //
-// File
+// Single file
 //
-// This will return a single file by its id
+// This will returns a single file by its id
 //
 // Schemes: http
 // Responses:
 // 	200: fileResponse
 
-// swagger:route DELETE /api/files/{id} files idParam
+// swagger:parameters file
+type fileRequest struct {
+	// The file id
+	// unique:true
+	// in:path
+    ID string `json:"id"`
+}
+
+// Single file
+// swagger:response fileResponse
+type fileResponse struct {
+	// in:body
+	Body file.GetFileByIDResponse
+}
+
+// swagger:route GET /files/{id}/ancestors files fileAncestors
+//
+// Returns a file ancestors
+//
+// This will returns all file ancestors
+//
+// Schemes: http
+// Responses:
+// 	200: fileAncestorsResponse
+
+// swagger:parameters fileAncestors
+type fileAncestorsRequest struct {
+	// The file id to get is ancestors
+	// unique:true
+	// in:path
+    ID string `json:"id"`
+}
+
+// File ancestors
+// swagger:response fileAncestorsResponse
+type fileAncestorsResponse struct {
+	// in:body
+	Ancestors []string
+}
+
+// swagger:route DELETE /files/{id} files deleteFile
 //
 // Delete file
 //
-// This will remove file by its id
+// This will deletes the file according to its ID
 //
 // Schemes: http
 // responses:
-//	200: idResponse
+//	200: DeleteResponse
+
+// swagger:parameters deleteFile
+type deleteFileRequest struct {
+	// The file id to delete
+	// unique:true
+	// in:path
+    ID string `json:"id"`
+}
+
+// An array of ids
+// swagger:response DeleteResponse
+type DeleteResponse struct {
+	// in:body
+	ID []string
+	
+}
+
+// swagger:route PUT /files/{id} files updateFile
+//
+// Update file
+//
+// This will update the file according to its ID
+//
+// Schemes: http
+// responses:
+//	200: UpdateResponse
+
+// swagger:parameters updateFile
+type updateRequest struct {
+	// The file id to update
+	// unique:true
+	// in:path
+	ID string `json:"id"`
+
+	// The partial file to update
+	// in:body
+    PartialFile file.GetFileByIDResponse
+}
+
+// An array of ids Of failed files
+// swagger:response UpdateResponse
+type UpdateResponse struct {
+	// in:body
+	FailedFilesID []string
+	
+}
+
+// swagger:route PUT /files files updateFiles
+//
+// Update files
+//
+// Updates many files with the same value.
+//
+// Schemes: http
+// responses:
+//	200: UpdateResponse
+
+// swagger:parameters updateFiles
+type updateFilesRequest struct {
+	// Array of ids to update.
+	// in:body
+	IDs []string
+
+	// The partial file to update
+	// in:body
+    PartialFile file.GetFileByIDResponse
+}
+
+// An array of ids Of failed files
+// swagger:response UpdateResponse
+type UpdateResponse struct {
+	// in:body
+	FailedFilesID []string
+}
+
+
+
+
